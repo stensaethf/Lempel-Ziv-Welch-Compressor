1f8e4cb9e1e1961a5fc7df1df3ebed7c0c5adf95
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-28 20:54:51 -0500

    initial setup

---
+++ lzw.py
@@ -0,0 +1,22 @@
+'''
+lzw.py
+Frederik Roenn Stensaeth
+10.28.15
+
+A Python implementation of the Lempel-Ziv-Welch compression algorithm.
+'''
+
+
+def compress():
+	# Code
+
+def decompress():
+	# Code
+
+
+def main():
+	# Code
+
+
+if __name__ == '__main__':
+	main()
