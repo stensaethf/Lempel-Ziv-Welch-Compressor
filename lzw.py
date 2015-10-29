48cfa068b314cedce64ae48dc0d94e356ba40a7a
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-28 21:55:35 -0500

    decompress() should work --> tested with abananabanana and TOTOTOOTSTOOTOTS

--- lzw.py
+++ lzw.py
@@ -7,6 +7,7 @@ A Python implementation of the Lempel-Ziv-Welch compression algorithm.
 '''
 
 # Any imports?
+import sys
 
 def compress(text):
 	"""
@@ -32,15 +33,15 @@ def compress(text):
 	# longest substring you have seen that is in your table.
 	for char in text:
 		total = value + char
+		print total
 		index += 1
 		# If we have seen total before we want to make it our value (aka we
 		# want to remember it) and move on to the next character. However,
 		# we also need to check if we have reached the end of the string. If
 		# we have reached the end, we add the total to our compressed list.
 		if total in table:
-			if index == len(text):
-				compressed_lst.append(table[total])
 			value = total
+
 		# However, if we have not seen total before, we add value (the
 		# substring we had remembered) to the ditionary and we add total
 		# to the dictionary (because we have not seen it before). We then
@@ -49,6 +50,9 @@ def compress(text):
 			compressed_lst.append(table[value])
 			table[total] = len(table)
 			value = char
+
+		if index == len(text):
+			compressed_lst.append(table[value])
 		# print(total) # For testing purposes.
 
 	return compressed_lst
@@ -61,12 +65,62 @@ def decompress(compressed_lst):
 	@params: compressed string (list of ints).
 	@return: decompressed string.
 	"""
-	# Code
-	# Xx
+	# We start by reconstructing the dictionary we used to compress the
+	# string. However, now the keys are the integers and the values are the 
+	# strings.
+	table = {}
+	for i in range(256):
+		table[i] = chr(i)
+
+	prev = str(chr(compressed_lst[0]))
+	compressed_lst = compressed_lst[1:]
+	decompressed_str = prev
+	# Loops over element in the compressed list so that we can decompress it.
+	for element in compressed_lst:
+		if element not in table:
+			printError(1)
+		elif element in table:
+			print(element)
+			string = table[element]
+		elif element == len(table):
+			print('entered!')
+			print prev
+			print prev[0]
+			string = prev + prev[0]
+		else:
+			printError(1)
+		decompressed_str += string
+
+		table[len(table)] = prev + string[0]
+
+		prev = string
+
+	return decompressed_str
+
+def printError(num):
+	if num == 1:
+		print('Error. Invalid compressed list given to decompress().')
+	else:
+		print('Error.')
+	print('Usage: $ lzw.py <string to be compressed> | <compressed list>')
+	sys.exit()
 
 def main():
-	# Code
-	print(compress('abananabanana'))
+	comp = compress('abananabanana')
+	print(comp)
+	decomp = decompress(comp)
+	print(decomp)
+
+	comp = compress('TOTOTOOTSTOOTOTS')
+	print(comp)
+	decomp = decompress(comp)
+	print(decomp)
+	# if len(sys.argv) != 1:
+	# 	printError(0)
+	# if sys.argv.type == list:
+	# 	Xx
+	# elif sys.argv.type == str:
+	# 	Xx
 
 
 if __name__ == '__main__':
