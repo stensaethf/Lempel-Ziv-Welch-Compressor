3658a8ae8b0c4794a774a3d2d9d8b5a66e0cafb6
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-28 21:24:25 -0500

    compress() commented

--- lzw.py
+++ lzw.py
@@ -27,18 +27,29 @@ def compress(text):
 
 	value = ''
 	index = 0
+	# Loop over each individual character in the text and keep track of where
+	# in the string you are (using the value index). Value keeps track of the
+	# longest substring you have seen that is in your table.
 	for char in text:
 		total = value + char
 		index += 1
+		# If we have seen total before we want to make it our value (aka we
+		# want to remember it) and move on to the next character. However,
+		# we also need to check if we have reached the end of the string. If
+		# we have reached the end, we add the total to our compressed list.
 		if total in table:
 			if index == len(text):
 				compressed_lst.append(table[total])
 			value = total
+		# However, if we have not seen total before, we add value (the
+		# substring we had remembered) to the ditionary and we add total
+		# to the dictionary (because we have not seen it before). We then
+		# move on to remembering the most recent character.
 		else:
 			compressed_lst.append(table[value])
 			table[total] = len(table)
 			value = char
-		print(total)
+		# print(total) # For testing purposes.
 
 	return compressed_lst
 
